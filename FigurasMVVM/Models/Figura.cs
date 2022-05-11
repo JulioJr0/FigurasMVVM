3fceb6add626ddc62b005a4daf04c12651cf1596
JulioJr0 <julio92reyes@hotmail.com>
2022-05-11 03:04:17 -0500

    Subiendo el Proyecto Figuras

---
+++ FigurasMVVM/Models/Figura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasMVVM.Models
+{
+    public class Figura
+    {
+        //Campos
+        public String CharacterName { get; set; } = "";
+        public String Brand { get; set; } = "";
+        public String Details { get; set; } = "";
+        public String RelaseDate { get; set; } = "";
+        public int ListPrice { get; set; } = 0;
+        public String Imagen { get; set; } = "";
+
+    }
+}
---
+++ FigurasMVVM/ViewModels/FigurasViewModel.cs
@@ -0,0 +1,70 @@
+﻿using FigurasMVVM.Models;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FigurasMVVM.ViewModels
+{
+    public class FigurasViewModel : INotifyPropertyChanged
+    {
+        //Modelos
+        public ObservableCollection<Figura> Lista { get; set; } = new ObservableCollection<Figura>();
+        //
+        void PropertyChange(string? propiedad = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propiedad));
+        }
+        public Figura? Figura { get; set; }
+        public string Vista { get; set; } = "Ver";
+        public string? Error { get; set; }
+
+        //Commands
+        public ICommand CancelarCommand { get; set; }
+        public ICommand CambiarVistaCommand { get; set; }
+        public ICommand AgregarCommand { get; set; }
+        public ICommand GuardarCommand { get; set; }
+        public ICommand EliminarCommand { get; set; }
+        //CTR
+        public FigurasViewModel()
+        {
+            CancelarCommand = new RelayCommand(Cancelar);
+            CambiarVistaCommand = new RelayCommand<String>(CambiarVista);
+            AgregarCommand = new RelayCommand(Agregar);
+            GuardarCommand = new RelayCommand(Guardar);
+            EliminarCommand = new RelayCommand(Eliminar);
+        }
+
+        private void Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Guardar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Agregar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CambiarVista(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Cancelar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
